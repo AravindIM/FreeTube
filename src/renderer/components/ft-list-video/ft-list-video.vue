6c41f04aea3b0d0762e39e0eb02c836b90c8b83f
AravindIM <mail.aravindim@proton.me>
2023-03-05 11:57:20 +0530

    Add clean page views

--- src/renderer/components/ft-list-video/ft-list-video.vue
+++ src/renderer/components/ft-list-video/ft-list-video.vue
@@ -44,16 +44,6 @@
         :size="appearance === `watchPlaylistItem` ? 12 : 16"
         @click="handleExternalPlayer"
       />
-      <ft-icon-button
-        v-if="!isLive"
-        :title="$t('Video.Save Video')"
-        :icon="['fas', 'star']"
-        class="favoritesIcon"
-        :theme="favoriteIconTheme"
-        :padding="appearance === `watchPlaylistItem` ? 5 : 6"
-        :size="appearance === `watchPlaylistItem` ? 14 : 18"
-        @click="toggleSave"
-      />
       <div
         v-if="addWatchedStyle"
         class="videoWatched"
